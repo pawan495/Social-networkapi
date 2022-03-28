c778cc29c05599fa500bc59d4343ddc39e98600c
pawan495 <pawanpreetkaur495@gmail.com>
2022-03-27 20:20:26 -0400

    write server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3081;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
